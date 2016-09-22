911b68e1e7317b1e151e2d2ca96eeab3949f7bc1
ashwinimnnit <ashwinidx@gmail.com>
2016-09-22 12:21:51 +0530

    Update component-add-user-item.js

--- dev/js/components/component-add-user-item.js
+++ dev/js/components/component-add-user-item.js
@@ -20,8 +20,8 @@ var AddUserItem = React.createClass({
 
   // post user data
   postUserDeatils: function() {
- 	  var url = "http://localhost:4000/user_profile/1/items.json";
- 	  var formdata = new FormData();
+    var url = "http://localhost:4000/user_profile/1/items.json";
+    var formdata = new FormData();
     var inputs = document.getElementById("addItem-form");
     var self = this;
     for(var input in inputs) {
@@ -35,21 +35,21 @@ var AddUserItem = React.createClass({
       		}
       }
     }
- 	  axios({
+    axios({
       method: 'post',
       url: url,
       processData: false,
       contentType: false,
       data:  formdata,
-   	}).then(function (response) {
-   			if (response.data.item.status == 500){
-   				alert ("some error occured")
-   			}
-   			else if (response.data.item.status == 200){
-   				alert("item created")
-   			}
+    }).then(function (response) {
+         if (response.data.item.status == 500){
+   	   alert ("some error occured")
+         }
+   	 else if (response.data.item.status == 200){
+   	  alert("item created")
+         }
         {self.props.itemCreated}  
-   		 })
+       })
   },
 
   addItemForm: function(){
