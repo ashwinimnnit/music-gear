4b9c85906da3ed4c9eb872be8fcb738c75e75eef
ashwinimnnit <ashwinidx@gmail.com>
2016-11-21 18:00:37 +0530

    user address

--- dev/js/containers/user-address-container.js
+++ dev/js/containers/user-address-container.js
@@ -13,15 +13,52 @@ var UserAddressesContainer = React.createClass({
 
 	displayAddresss: function (){
        var addressArray = []
+       var array = []
        addressArray = this.props.userAddresses.addresses
+       console.log("adddress ki id ", addressArray)
        if (typeof addressArray != "undefined"){
-       	console.log("-----address", addressArray)
         for (var address in addressArray) {
-        	console.log("----0000", addressArray[address])
+        	array.push(
+                <div key = {address} className = "address">
+                <form id = {"form-"+address+""}>
+                <div className = "row">
+                  Name: <input type="text" name = "name" defaultValue = "ashwini" disabled className ="disabled inputs"/>
+                </div>
+        		<div className = "row">
+        		  House No: <input type="text" name = "house_no" defaultValue = {addressArray[address].house_no}  disabled />
+        		</div>
+                <div className = "row">
+                  Street: <input type="text" name = "street" defaultValue = {addressArray[address].street} disabled />
+                </div>
+                <div className = "row"> 
+                  State: <input type="text" name = "city" defaultValue = {addressArray[address].city} disabled />
+                </div>
+                <div className = "row">
+                  Pin: <input type="text" name = "state" defaultValue = {addressArray[address].state} disabled />
+                </div>
+                <div className = "row">
+                  Phone: <input type="text" name = "pin" defaultValue = {addressArray[address].pin} disabled />
+                </div>
+                   <img src="http://localhost:3000/images/edit-image.jpg" 
+                       className="edit-image-button" 
+                       onClick = {this.displayEditableFields.bind(this, addressArray[address])}/>
+                </form>   
+                </div>
+        		)
         }
       }
+      return (<div className = "user-address"> {array}</div>)
 	},
 
+    displayEditableFields: function(userAddress){
+        var form = document.getElementById("form-"+userAddress.id+"");
+        var elements = form.elements;
+         for (var i = 0, len = elements.length; i < len; ++i) {
+            elements[i].disabled = false;
+            elements[i].className = "editable-form"
+           } 
+    },
+   
 	render: function(){
       return(
       	<div>{this.displayAddresss()} </div>
